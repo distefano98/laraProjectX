09e6dfeea5d531612fafc31a229f5b63371c163d
luca <lucads02@libero.it>
2020-05-15 16:37:55 +0200

    homepage header :)

---
+++ laraProjX/resources/views/Homepage.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+        <title> WLCC </title>
+    </head>
+   
+    
+    
+    <body>
+        <header class="header" >
+            @include('layout/headerPublic')
+        </header>
+        
+        
+        
+        
+        
+    </body>
+    
+    
+    
+    
+</html>
---
+++ laraProjX/resources/views/layout/headerPublic.blade.php
@@ -0,0 +1,13 @@
+<a href="{{ route('homepage') }}" class="header__logo" > <img width="250" src="{{ asset('css/image/logo.png') }}">  </a>
+   
+    <ul class="header__menu">
+                <li class="header__menu__item"> <a href="{{ route('homepage') }}"> HOME </a></li>
+                <li class="header__menu__item"> <a href="{{ route('catalog') }}"> CATALOGO </a></li>
+                <li class="header__menu__item"> <a href="{{ route('who') }}"> CHI SIAMO </a></li>
+                <li class="header__menu__item"> <a href="{{ route('where') }}"> DOVE SIAMO </a></li>
+                <li class="header__menu__item"> <a href="{{ route('login') }}"> ACCEDI </a></li>
+    </ul>
+            
+      
+        
+        
