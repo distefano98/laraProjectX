930a3a373813cb2dffe668c9f6c9a29554be57e3
luca <lucads02@libero.it>
2020-05-20 11:09:42 +0200

    risolto problema dello sconto che non si vedeva

--- laraProjX/resources/views/helpers/productPrice.blade.php
+++ laraProjX/resources/views/helpers/productPrice.blade.php
@@ -1,6 +1,7 @@
-<p class="price"> {{ number_format($product->getPrice($product->sconto), 2, ',', '.') }} € </p>
 
-@if ($product->discounted == 1)
-<p class="discprice"> Valore <del>{{ number_format($product->getPrice(false), 2, ',', '.') }} €</del><br>
+<p class="price">  {{ number_format($product->getPrice($product->sconto), 2, ',', '.') }} € </p>
+
+@if ($product->sconto == 1)
+<p class="discprice"> Prezzo non scontato <del>{{ number_format($product->getPrice(false), 2, ',', '.') }} €</del><br>
     Sconto {{ $product->scontoPerc }}%</p>
 @endif
