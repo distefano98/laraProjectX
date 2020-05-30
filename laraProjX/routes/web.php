2721b772090ce4d023310bed9dcb8d9316c0cd50
CristianCingo <52571810+CristianCingo@users.noreply.github.com>
2020-05-30 14:53:14 +0200

    Revert "cancellazione prodotto"
    This reverts commit 7cf9ed7d3946b8d7ab7cef82a0a121a36707a38a.

--- laraProjX/routes/web.php
+++ laraProjX/routes/web.php
@@ -105,9 +105,6 @@ Route::get('/staff/changeproduct/{prodId}', 'StaffController@changeProduct')
 Route::post('/staff/changeproduct/{prodId}', 'StaffController@updateProduct')
         ->name('changeproduct.update');
 
-Route::get('/deleteproduct/{prodId}','StaffController@deleteProduct')
-        ->name('deleteproduct');
-
 //admin
 Route::get('/admin', 'AdminController@index')
         ->name('admin')->middleware('can:isAdmin');
